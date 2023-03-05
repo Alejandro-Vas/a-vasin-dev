d578554a0101a369263595a114108e97bb6663fa
Aleksandr Vasin <zended@yandex.ru>
2023-03-05 18:01:03 +0300

    feat: cat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import cat from '../../public/cat.jpg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,10 +9,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <h1>
+          WELCOME
+        </h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -20,12 +20,12 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src={cat}
+              alt="CAT"
+              width={600}
+              height={400}
+              placeholder="blur"
+              // blurDataURL='https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg'
             />
           </a>
         </div>
