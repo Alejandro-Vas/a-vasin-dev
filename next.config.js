c953fedebee8f54ab6c07b5e984787c0f4f001f9
Aleksandr Vasin <zended@yandex.ru>
2023-03-06 11:42:29 +0300

    fix; pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    reactStrictMode: true,
     appDir: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
