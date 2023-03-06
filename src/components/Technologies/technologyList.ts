91e8e6f6b2650c33fc38bd2287c6961ec9ff4a5f
Aleksandr Vasin <zended@yandex.ru>
2023-03-06 13:09:02 +0300

    feat: icons

---
+++ src/components/Technologies/technologyList.ts
@@ -0,0 +1,115 @@
+import argoCd from '@assets/icons/argoCd.svg';
+import bootstrap from '@assets/icons/bootstrap.svg';
+import css3 from '@assets/icons/css3.svg';
+import docker from '@assets/icons/docker.svg';
+import express from '@assets/icons/express.svg';
+import git from '@assets/icons/git.svg';
+import gitlab from '@assets/icons/gitlab.svg';
+import html5 from '@assets/icons/html5.svg';
+import javascript from '@assets/icons/javascript.svg';
+import jest from '@assets/icons/jest.svg';
+import kubernetes from '@assets/icons/kubernetes.svg';
+import materialUI from '@assets/icons/materialUI.svg';
+import nextJs from '@assets/icons/nextJs.svg';
+import nodeJs from '@assets/icons/nodeJs.svg';
+import npm from '@assets/icons/npm.svg';
+import react from '@assets/icons/react.svg';
+import redux from '@assets/icons/redux.svg';
+import stylus from '@assets/icons/stylus.svg';
+import tailwindCss from '@assets/icons/tailwindCss.svg';
+import typescript from '@assets/icons/typescript.svg';
+import vscode from '@assets/icons/vscode.svg';
+import vueJs from '@assets/icons/vueJs.svg';
+
+const technologyList = [
+  {
+    label: 'ArgoCD',
+    icon: argoCd,
+  },
+  {
+    label: 'Bootstrap',
+    icon: bootstrap,
+  },
+  {
+    label: 'CSS',
+    icon: css3,
+  },
+  {
+    label: 'Docker',
+    icon: docker,
+  },
+  {
+    label: 'Express',
+    icon: express,
+  },
+  {
+    label: 'Git',
+    icon: git,
+  },
+  {
+    label: 'Gitlab',
+    icon: gitlab,
+  },
+  {
+    label: 'HTML5',
+    icon: html5,
+  },
+  {
+    label: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    label: 'Jest',
+    icon: jest,
+  },
+  {
+    label: 'Kubernetes',
+    icon: kubernetes,
+  },
+  {
+    label: 'Material UI',
+    icon: materialUI,
+  },
+  {
+    label: 'Next.js',
+    icon: nextJs,
+  },
+  {
+    label: 'Node.js',
+    icon: nodeJs,
+  },
+  {
+    label: 'NPM',
+    icon: npm,
+  },
+  {
+    label: 'React',
+    icon: react,
+  },
+  {
+    label: 'Redux',
+    icon: redux,
+  },
+  {
+    label: 'Stylus',
+    icon: stylus,
+  },
+  {
+    label: 'TailwindCSS',
+    icon: tailwindCss,
+  },
+  {
+    label: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    label: 'VsCode',
+    icon: vscode,
+  },
+  {
+    label: 'Vue.js',
+    icon: vueJs,
+  },
+];
+
+export default technologyList;
