e6083767a82e5130c79a5b25321a772b7608136d
Aleksandr Vasin <zended@yandex.ru>
2023-03-07 16:14:14 +0300

    feat: paper

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -8,30 +8,32 @@ function Experience() {
         <h2 className="textAlignCenter">
           EXPERIENCE
         </h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
-        Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
-        dolores doloribus dignissimos!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
-        expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
-        Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
-        dolores doloribus dignissimos!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
-        expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
+        <div className="paper">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
+          Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
+          dolores doloribus dignissimos!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
+          expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
-        Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
-        dolores doloribus dignissimos!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
-        expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
+          Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
+          dolores doloribus dignissimos!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
+          expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Veritatis eum voluptate tempora laborum nesciunt dolore accusamus et suscipit.
+          Expedita totam itaque repellat? Quo autem minus blanditiis saepe,
+          dolores doloribus dignissimos!
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Omnis eveniet sapiente soluta amet suscipit dignissimos voluptas excepturi quia maxime ex,
+          expedita eum quod placeat harum. Fuga minima accusamus ad dolor.
+        </div>
       </div>
     </div>
   );
