68b2fbb99802deda03db24ddf170c89334537aaf
Aleksandr Vasin <zended@yandex.ru>
2023-03-10 18:29:00 +0300

    feat: carousel

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,21 +2,39 @@
 
 import { ANCHORS } from '@constants/index';
 import Carousel from 'nuka-carousel';
-import Experience from '@components/Experience';
 import Button from '@components/Button';
 
 import GithubIcon from '@assets/icons/technologies/github.svg';
-
+import newsApp from '@assets/images/projects/newsApp.png';
+import personalSite from '@assets/images/projects/personalSite.png';
+import jsonViewer from '@assets/images/projects/jsonViewer.png';
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 const projects = [
   {
     title: 'NEWS APP',
-    image: '',
-    description: 'new ',
+    image: newsApp,
+    description: '',
     technologies: 'JavaScript, TypeScript, React, Redux, Material UI',
     sourceCode: 'https://github.com/Alejandro-Vas/news-app',
     demoUrl: 'https://lighthearted-custard-5024f1.netlify.app',
+  },
+  {
+    title: 'PERSONAL SITE',
+    image: personalSite,
+    description: '',
+    technologies: 'JavaScript, TypeScript, React, NextJS, SCSS',
+    sourceCode: 'https://github.com/Alejandro-Vas/a-vasin-dev',
+    demoUrl: 'https://unique-monstera-ba326c.netlify.app',
+  },
+  {
+    title: 'JSON VIEWER (NPM PACKAGE)',
+    image: jsonViewer,
+    description: '',
+    technologies: 'JavaScript, TypeScript, React',
+    sourceCode: 'https://github.com/Alejandro-Vas/JSON-viewer',
+    demoUrl: 'https://jade-hamster-a6833c.netlify.app/',
   },
 ];
 
@@ -30,38 +48,52 @@ function Projects() {
 
       <div className="paper">
         <Carousel
-          defaultControlsConfig={{ prevButtonText: '<', nextButtonText: '>' }}
+          defaultControlsConfig={{
+            prevButtonText: '<',
+            nextButtonText: '>',
+            pagingDotsStyle: {
+              padding: '4px',
+            },
+          }}
         >
           {projects.map(({
-            title, demoUrl, sourceCode, technologies,
+            title, demoUrl, sourceCode, technologies, image,
           }) => (
-            <>
-              <div key={title}>
-                <h2>
-                  {title}
-                </h2>
+            <div className={styles.container} key={title}>
+              <div className={styles.text}>
+                <div>
+                  <h2>
+                    {title}
+                  </h2>
+                </div>
+
+                <div>
+                  {technologies}
+                </div>
+
+                <div className={styles.links}>
+                  <Button>
+                    <a href={demoUrl} target="_blank" rel="noreferrer">
+                      DEMO
+                    </a>
+                  </Button>
+
+                  <div className={styles.githubIcon}>
+                    <a href={sourceCode} target="_blank" rel="noreferrer">
+                      <GithubIcon />
+                    </a>
+                  </div>
+                </div>
               </div>
 
-              <div>
-                {technologies}
-              </div>
-
-              <Button>
-                <a href={demoUrl} target="_blank" rel="noreferrer">
-                  DEMO
-                </a>
-              </Button>
-
-              <div className={styles.githubIcon}>
-                <a href={sourceCode} target="_blank" rel="noreferrer">
-                  <GithubIcon />
-                </a>
-              </div>
-
-            </>
+              <Image
+                src={image}
+                alt="Project preview"
+                height={300}
+                placeholder="blur"
+              />
+            </div>
           ))}
-          <Experience />
-          <Experience />
         </Carousel>
       </div>
     </div>
